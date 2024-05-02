96bebc427d2e067c4f4afa61bbcb37d4759740c7
mtho <mtho.dawu@gmail.com>
2024-05-03 00:17:24 +0200

    Added BlogCarousel, and first Post

--- app/page.js
+++ app/page.js
@@ -1,21 +1,18 @@
-import Image from "next/image";
-import { FaLaptop } from "react-icons/fa";
+// Import statements...
+import Home, { BlogCarousel, Post } from './defs.js'; // Importing the default export and named export
 
-export default function Home() {
+export default function MyComponent() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <section className="min-h-screen px-10">
-            <nav className="flex justify-between items-center py-10 mb-12">
-                <h1 className="text-xl">Developed by Mtho</h1>
-                <ul className="flex items-center">
-                    <li>
-                      <FaLaptop className="cursor-pointer tex"/>
-                    </li>
-                    <li></li>
-                </ul>
-            </nav>
-
-        </section>
-    </main>
+    <><section>
+      <div>
+        <Home />
+        <BlogCarousel />
+      </div>
+    </section>
+      <section>
+        <div>
+          <Post />
+        </div>
+      </section></>
   );
 }
